6b85819fd6ea72bda816ee10ca310c73ee48b016
mashfied <victoreslieh@gmail.com>
2024-11-12 14:48:00 +0300

    updates

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { SidebarWrapper } from './Sidebar.styled';
-
-const Sidebar = () => (
- <SidebarWrapper>
-    Sidebar Component
- </SidebarWrapper>
-);
+import React from "react";
+import PropTypes from "prop-types";
+import { SidebarWrapper } from "./Sidebar.styled";
+import { NavLink } from "react-router-dom";
+import logo from './Pixify.png'
+function Sidebar() {
+  return (
+    <SidebarWrapper className="sidebar">
+      <div className="nav-container">
+        <div className="logo-container">
+          <img src={logo} alt="logo" className="profile_image" />
+        </div>
+        <div className="navigators">
+          <NavLink to={"/"} className="nav-link">
+            Home
+          </NavLink>
+          <NavLink to={"/Search"} className="nav-link">
+            Search
+          </NavLink>
+          <NavLink to={"/Myprofile"} className="nav-link">
+            My Profile
+          </NavLink>
+        </div>
+      </div>
+    </SidebarWrapper>
+  );
+}
 
 Sidebar.propTypes = {};
 
