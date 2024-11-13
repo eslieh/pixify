3d7b5c8aa36edd0ae74dab6e2c3ba66bc61e132f
mashfied <victoreslieh@gmail.com>
2024-11-13 23:49:08 +0300

    rightbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar/Sidebar';
+import Rightbar from './components/Rightbar/Rightbar';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <>
         <Sidebar />
+        <Rightbar />
+        </>
       </div>
     </Router>
   );
