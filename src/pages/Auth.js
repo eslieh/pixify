817f13b37e8e8c30af1fd1a73d6ec336e224bf81
mashfied <victoreslieh@gmail.com>
2024-11-13 16:10:13 +0300

    fixed auth

---
+++ src/pages/Auth.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from '../components/Login/Login';
+import Signup from '../components/Signup/SignUp';
+
+function Auth(){
+    return (
+        <>
+        <Login/>
+        <Signup/>
+        </>
+        
+    )
+};
+
+export default Auth;
